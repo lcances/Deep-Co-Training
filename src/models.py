96f2926e1839761cf8ff111b3e6cd0838a94569e
leocances <leocances@gmail.com>
2019-12-20 10:13:41 +0100

    add a second model (bigger: > 200k param)

--- src/models.py
+++ src/models.py
@@ -31,3 +31,31 @@ class cnn(nn.Module):
         x = self.classifier(x)
 
         return x
+
+
+class cnn2(nn.Module):
+    def __init__(self):
+        super(cnn2, self).__init__()
+
+        self.features = nn.Sequential(
+            ConvPoolReLU(1, 24, 5, 1, 1, (4, 2), (4, 2)),
+            ConvPoolReLU(24, 48, 5, 1, 1, (4, 2), (4, 2)),
+            ConvReLU(48, 48, 5, 1, 1),
+        )
+
+        self.classifier = nn.Sequential(
+            nn.Flatten(),
+            nn.Dropout(0.5),
+            nn.Linear(1872, 64),
+            nn.ReLU(),
+            nn.Dropout(0.5),
+            nn.Linear(64, 10)
+        )
+
+    def forward(self, x):
+        x = x.view(-1, 1, *x.shape[1:])
+
+        x = self.features(x)
+        x = self.classifier(x)
+
+        return x
