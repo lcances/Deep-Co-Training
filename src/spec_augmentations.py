0668ee82e6eb9e8581e2ebf2ce0c535f3c9ef25b
leocances <leocances@gmail.com>
2020-01-07 15:41:52 +0100

    Add FractalTimeDropout (tested)

--- src/spec_augmentations.py
+++ src/spec_augmentations.py
@@ -48,72 +48,67 @@ class Augmentation:
 
 class FractalTimeStretch(Augmentation):
     def __init__(self, ratio, intra_ratio: float = 0.3, rate: tuple = (0.8, 1.2),
-                 min_column_size: int = None, max_column_size: int = None):
+                 min_chunk_size: int = None, max_chunk_size: int = None):
         """
         Args:
             ratio: The probability of applying the augmentation of the file
             intra_ratio (float): The probability to apply time stretch on one column
             rate (tuple): The min and max stretch value to use
-            min_column_size (int): the minimum size of a column
-            max_column_size (int): The maximum size of a column
+            min_chunk_size (int): the minimum size of a column
+            max_chunk_size (int): The maximum size of a column
         """
         super().__init__(ratio)
 
         # This values, if set to None, will be automatically computed when needed
-        self.min_column_size = min_column_size  # 1% of the total length
-        self.max_column_size = max_column_size  # 10% of the total length
+        self.min_chunk_size = min_chunk_size  # 1% of the total length
+        self.max_chunk_size = max_chunk_size  # 10% of the total length
 
         # ratio to apply or not the stretching on each columns (independantly)
         self.intra_ratio = intra_ratio
         self.rate = rate
 
     def _apply(self, data):
-        S_im = Image.fromarray(data)
-        (w, h) = S_im.size
+        (h, w) = data.shape
 
         # Compute min and max column size if needed
-        self.min_column_size = int(w * 0.01) if self.min_column_size is None else self.min_column_size
-        self.max_column_size = int(w * 0.1) if self.max_column_size is None else self.max_column_size
+        self.min_chunk_size = int(w * 0.01) if self.min_chunk_size is None else self.min_chunk_size
+        self.max_chunk_size = int(w * 0.1) if self.max_chunk_size is None else self.max_chunk_size
 
-        # Split the spectro into many small columns (random size)
+        # Split the spectro into many small chunks (random size)
         freq, temps = data.shape
-        columns_width = []
-        columns = []
+        chunks_width = []
+        chunks = []
 
-        while sum(columns_width) < w:
-            width = np.random.randint(self.min_column_size, self.max_column_size)
+        while sum(chunks_width) < w:
+            width = np.random.randint(self.min_chunk_size, self.max_chunk_size)
 
-            current_index = sum(columns_width)
-            columns.append(data[:, current_index:current_index+width])
+            current_index = sum(chunks_width)
+            chunks.append(data[:, current_index:current_index+width])
 
-            columns_width.append(width)
+            chunks_width.append(width)
 
         # Apply time stretch to each column (intra_ratio)
-        ratios = np.random.uniform(0, 1, len(columns))
-        stretched_columns = []
+        ratios = np.random.uniform(0, 1, len(chunks))
+        stretched_chunks = []
 
-        print(columns_width)
-        print(ratios)
-        print(len(columns))
-        for ratio, width, column in zip(ratios, columns_width, columns):
+        for ratio, width, column in zip(ratios, chunks_width, chunks):
             if ratio <= self.intra_ratio:
                 rate = np.random.uniform(*self.rate)
                 column = Image.fromarray(column)
 
-                print(width)
                 stretched_column = Image.Image.resize(
                     column,
                     (int(width * rate), h),
                     random_interpolation()
                 )
 
-                stretched_columns.append(np.array(stretched_column))
+                stretched_chunks.append(np.array(stretched_column))
 
             else:
-                stretched_columns.append(column)
+                stretched_chunks.append(column)
 
         # Final resized to original dimension
-        stretched_S = np.concatenate(stretched_columns, axis=1)
+        stretched_S = np.concatenate(stretched_chunks, axis=1)
 
         tmp = Image.fromarray(stretched_S)
         tmp = Image.Image.resize(tmp, (temps, freq), Image.LANCZOS)
@@ -125,32 +120,31 @@ class FractalTimeStretch(Augmentation):
 
 class FractalFreqStretch(Augmentation):
     def __init__(self, ratio, intra_ratio: float = 0.3, rate: tuple = (0.8, 1.2),
-                 min_column_size: int = None, max_column_size: int = None):
+                 min_chunk_size: int = None, max_chunk_size: int = None):
         """
         Args:
             ratio: The probability of applying the augmentation of the file
             intra_ratio (float): The probability to apply time stretch on one column
             rate (tuple): The min and max stretch value to use
-            min_column_size (int): the minimum size of a column
-            max_column_size (int): The maximum size of a column
+            min_chunk_size (int): the minimum size of a column
+            max_chunk_size (int): The maximum size of a column
         """
         super().__init__(ratio)
 
         # This values, if set to None, will be automatically computed when needed
-        self.min_column_size = min_column_size  # 1% of the total length
-        self.max_column_size = max_column_size  # 10% of the total length
+        self.min_chunk_size = min_chunk_size  # 1% of the total length
+        self.max_chunk_size = max_chunk_size  # 10% of the total length
 
         # ratio to apply or not the stretching on each columns (independantly)
         self.intra_ratio = intra_ratio
         self.rate = rate
 
     def _apply(self, data):
-        S_im = Image.fromarray(data)
-        (w, h) = S_im.size
+        (h, w) = data.shape
 
-        # Compute min and max column size if needed
-        self.min_column_size = int(h * 0.01) if self.min_column_size is None else self.min_column_size
-        self.max_column_size = int(h * 0.1) if self.max_column_size is None else self.max_column_size
+        # Compute min and max chunk size if needed
+        self.min_chunk_size = int(h * 0.01) if self.min_chunk_size is None else self.min_chunk_size
+        self.max_chunk_size = int(h * 0.1) if self.max_chunk_size is None else self.max_chunk_size
 
         # Split the spectro into many small chunks (random size)
         freq, temps = data.shape
@@ -158,39 +152,35 @@ class FractalFreqStretch(Augmentation):
         chunks = []
 
         while sum(chunk_width) < h:
-            width = np.random.randint(self.min_column_size, self.max_column_size)
+            width = np.random.randint(self.min_chunk_size, self.max_chunk_size)
 
             current_index = sum(chunk_width)
             chunks.append(data[current_index:current_index+width, :])
 
             chunk_width.append(width)
 
-        # Apply time stretch to each column (intra_ratio)
+        # Apply time stretch to each chunk (intra_ratio)
         ratios = np.random.uniform(0, 1, len(chunks))
-        stretched_columns = []
+        stretched_chunks = []
 
-        print(chunk_width)
-        print(ratios)
-        print(len(chunks))
-        for ratio, width, column in zip(ratios, chunk_width, chunks):
+        for ratio, width, chunk in zip(ratios, chunk_width, chunks):
             if ratio <= self.intra_ratio:
                 rate = np.random.uniform(*self.rate)
-                column = Image.fromarray(column)
+                chunk = Image.fromarray(chunk)
 
-                print(width)
                 stretched_column = Image.Image.resize(
-                    column,
+                    chunk,
                     (w, int(width * rate)),
                     random_interpolation()
                 )
 
-                stretched_columns.append(np.array(stretched_column))
+                stretched_chunks.append(np.array(stretched_column))
 
             else:
-                stretched_columns.append(column)
+                stretched_chunks.append(chunk)
 
         # Final resized to original dimension
-        stretched_S = np.concatenate(stretched_columns, axis=0)
+        stretched_S = np.concatenate(stretched_chunks, axis=0)
 
         tmp = Image.fromarray(stretched_S)
         tmp = Image.Image.resize(tmp, (temps, freq), Image.LANCZOS)
@@ -198,6 +188,60 @@ class FractalFreqStretch(Augmentation):
         final_S = np.array(tmp)
 
         return final_S
+
+
+class FractalTimeDropout(Augmentation):
+    def __init__(self, ratio, intra_ratio: float = 0.1,
+                 min_chunk_size: int = None, max_chunk_size: int = None,
+                 void: bool = True):
+        super().__init__(ratio)
+
+        # This values, if set to None, will be automatically computed when needed
+        self.min_column_size = min_chunk_size  # 1% of the total length
+        self.max_column_size = max_chunk_size  # 10% of the total length
+
+        # ratio to apply or not the stretching on each columns (independantly)
+        self.intra_ratio = intra_ratio
+        self.void = void
+
+    def _apply(self, data):
+        (h, w) = data.shape
+
+        # Compute min and max column size if needed
+        self.min_column_size = int(h * 0.01) if self.min_column_size is None else self.min_column_size
+        self.max_column_size = int(h * 0.1) if self.max_column_size is None else self.max_column_size
+
+        # Split the spectro into many small chunks (random size)
+        chunk_width = []
+        chunks = []
+
+        while sum(chunk_width) < w:
+            width = np.random.randint(self.min_column_size, self.max_column_size)
+
+            current_index = sum(chunk_width)
+            chunks.append(data[:, current_index:current_index+width])
+
+            chunk_width.append(width)
+
+        # each chunk have an <intra_ratio> chance of disapearing.
+        valid_mask = [False if x <= self.intra_ratio else True for x in np.random.uniform(0, 1, size=len(chunks))]
+
+        # minimum one chunk have to disapear
+        if sum(valid_mask) == len(chunks):
+            valid_mask[np.random.choice(range(len(chunks)))] = 0
+
+        print(valid_mask)
+        # reconstruct the signal using void or compacting it
+        reconstructed_S = []
+        for valid, chunk in zip(valid_mask, chunks):
+            if valid:
+                reconstructed_S.append(chunk)
+            else:
+                reconstructed_S.append(list(np.ones(chunk.shape) * chunk.min()))
+
+        reconstructed_S = np.concatenate(reconstructed_S, axis=1)
+
+        return reconstructed_S
 
 
 if __name__ == '__main__':
