22ce1826e4bb1b8632c4d039630c0bb8af040a5d
leocances <leocances@gmail.com>
2020-01-07 13:22:33 +0100

    Add and test FractalFreqStretch

--- src/spec_augmentations.py
+++ src/spec_augmentations.py
@@ -123,6 +123,83 @@ class FractalTimeStretch(Augmentation):
         return final_S
 
 
+class FractalFreqStretch(Augmentation):
+    def __init__(self, ratio, intra_ratio: float = 0.3, rate: tuple = (0.8, 1.2),
+                 min_column_size: int = None, max_column_size: int = None):
+        """
+        Args:
+            ratio: The probability of applying the augmentation of the file
+            intra_ratio (float): The probability to apply time stretch on one column
+            rate (tuple): The min and max stretch value to use
+            min_column_size (int): the minimum size of a column
+            max_column_size (int): The maximum size of a column
+        """
+        super().__init__(ratio)
+
+        # This values, if set to None, will be automatically computed when needed
+        self.min_column_size = min_column_size  # 1% of the total length
+        self.max_column_size = max_column_size  # 10% of the total length
+
+        # ratio to apply or not the stretching on each columns (independantly)
+        self.intra_ratio = intra_ratio
+        self.rate = rate
+
+    def _apply(self, data):
+        S_im = Image.fromarray(data)
+        (w, h) = S_im.size
+
+        # Compute min and max column size if needed
+        self.min_column_size = int(h * 0.01) if self.min_column_size is None else self.min_column_size
+        self.max_column_size = int(h * 0.1) if self.max_column_size is None else self.max_column_size
+
+        # Split the spectro into many small chunks (random size)
+        freq, temps = data.shape
+        chunk_width = []
+        chunks = []
+
+        while sum(chunk_width) < h:
+            width = np.random.randint(self.min_column_size, self.max_column_size)
+
+            current_index = sum(chunk_width)
+            chunks.append(data[current_index:current_index+width, :])
+
+            chunk_width.append(width)
+
+        # Apply time stretch to each column (intra_ratio)
+        ratios = np.random.uniform(0, 1, len(chunks))
+        stretched_columns = []
+
+        print(chunk_width)
+        print(ratios)
+        print(len(chunks))
+        for ratio, width, column in zip(ratios, chunk_width, chunks):
+            if ratio <= self.intra_ratio:
+                rate = np.random.uniform(*self.rate)
+                column = Image.fromarray(column)
+
+                print(width)
+                stretched_column = Image.Image.resize(
+                    column,
+                    (w, int(width * rate)),
+                    random_interpolation()
+                )
+
+                stretched_columns.append(np.array(stretched_column))
+
+            else:
+                stretched_columns.append(column)
+
+        # Final resized to original dimension
+        stretched_S = np.concatenate(stretched_columns, axis=0)
+
+        tmp = Image.fromarray(stretched_S)
+        tmp = Image.Image.resize(tmp, (temps, freq), Image.LANCZOS)
+
+        final_S = np.array(tmp)
+
+        return final_S
+
+
 if __name__ == '__main__':
     from datasetManager import DatasetManager
 
