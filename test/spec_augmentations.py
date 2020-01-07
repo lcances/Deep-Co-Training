7dc7431f560b57f94b156b59bcae3beb2997082f
leocances <leocances@gmail.com>
2020-01-07 13:22:33 +0100

    Add and test FractalFreqStretch

--- test/spec_augmentations.py
+++ test/spec_augmentations.py
@@ -15,19 +15,38 @@ print(type(test_raw_audio))
 test_melspectro = dataset.extract_feature(test_raw_audio, 22050)
 
 # FractalTimeStretch
-from src.spec_augmentations import FractalTimeStretch
-fts = FractalTimeStretch(1.0, intra_ratio=0.3, min_column_size=10, max_column_size=25)
+def test_FractalTimeStretch():
+    from src.spec_augmentations import FractalTimeStretch
+    fts = FractalTimeStretch(1.0, intra_ratio=0.3, min_column_size=10, max_column_size=25)
 
-fts_melspectro = fts(test_melspectro)
+    fts_melspectro = fts(test_melspectro)
 
-plt.figure(0, figsize=(20, 15))
-plt.subplot(3, 1, 1)
-plt.matshow(test_melspectro, fignum=0)
-plt.subplot(3, 1, 2)
-plt.matshow(fts_melspectro, fignum=0)
-plt.subplot(3, 1, 3)
-plt.matshow(test_melspectro - fts_melspectro, fignum=0)
-plt.show()
+    plt.figure(0, figsize=(20, 15))
+    plt.subplot(3, 1, 1)
+    plt.matshow(test_melspectro, fignum=0)
+    plt.subplot(3, 1, 2)
+    plt.matshow(fts_melspectro, fignum=0)
+    plt.subplot(3, 1, 3)
+    plt.matshow(test_melspectro - fts_melspectro, fignum=0)
+    plt.show()
 
 
+# FractalFreqStretch
+def test_FractalFreqStretch():
+    from src.spec_augmentations import FractalFreqStretch
+    fts = FractalFreqStretch(1.0, intra_ratio=1, min_column_size=20, max_column_size=40)
 
+    fts_melspectro = fts(test_melspectro)
+
+    plt.figure(0, figsize=(20, 15))
+    plt.subplot(3, 1, 1)
+    plt.matshow(test_melspectro, fignum=0)
+    plt.subplot(3, 1, 2)
+    plt.matshow(fts_melspectro, fignum=0)
+    plt.subplot(3, 1, 3)
+    plt.matshow(test_melspectro - fts_melspectro, fignum=0)
+    plt.show()
+
+
+# test_FractalTimeStretch()
+test_FractalFreqStretch()
