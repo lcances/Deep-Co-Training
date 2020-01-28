1017f2591e573b823d0f192a51e5ba575644b8b5
leocances <leocances@gmail.com>
2020-01-28 14:34:39 +0100

    create new Sequential layer that accept multiple input --> create model variant with adv BN

--- src/layers.py
+++ src/layers.py
@@ -2,6 +2,16 @@ import torch
 import torch.nn as nn
 
 
+class Sequential_adv(nn.Sequential):
+    def forward(self, *inputs):
+        for module in self._modules.values():
+            if type(inputs) == tuple:
+                inputs = module(*inputs)
+            else:
+                inputs = module(inputs)
+        return inputs
+
+
 class ConvPoolReLU(nn.Sequential):
     def __init__(self, in_size, out_size, kernel_size, stride, padding,
                  pool_kernel_size, pool_stride):
