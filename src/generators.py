b5de13e2ee36bf79f6a6fd5e59ff14f7e13e75b8
leocances <leocances@gmail.com>
2020-02-05 15:13:53 +0100

    refactoring and changing behaviour of classic Generator

--- src/generators.py
+++ src/generators.py
@@ -7,84 +7,71 @@ import numpy as np
 import pandas as pd
 
 from torch.utils import data
-from datasetManager import DatasetManager
-from src.signal_augmentations import SignalAugmentation
-from src.spec_augmentations import SpecAugmentation
+from signal_augmentations import SignalAugmentation
+from spec_augmentations import SpecAugmentation
 
 
 class Generator(data.Dataset):
-    def __init__(self, dataset, augments=()):
+    def __init__(self, dataset, train:bool = True, val:bool = False, sampling: float = 1.0, augments=()):
         super().__init__()
 
         self.dataset = dataset
+        self.train = train
+        self.val = val
+        self.sampling = sampling
         self.augments = augments
 
+        self._check_arguments()
+
         # dataset access (combine weak and synthetic)
-        self.x = self.dataset.audio["train"]
-        self.y = self.dataset.meta["train"]
+        if self.train:
+            self.x = self.dataset.audio["train"]
+            self.y = self.dataset.meta["train"]
+        elif self.val:
+            self.x = self.dataset.audio["val"]
+            self.y = self.dataset.meta["val"]
 
         self.filenames = list(self.x.keys())
-
-        # Validation
-        self.X_val, self.y_val = None, None
 
         # alias for verbose mode
         self.tqdm_func = self.dataset.tqdm_func
 
-    @property
-    def validation(self):
-        if self.X_val is not None and self.y_val is not None:
-            return self.X_val, self.y_val
+    def _check_arguments(self):
+        if sum([self.train, self.val]) != 1:
+            raise AssertionError("Train and val and mutually exclusive")
 
-        # Need to ensure that the data are store in the same order.
-        self.X_val, self.y_val = [], []
-        filenames = self.dataset.audio["val"].keys()
-
-        for filename in self.tqdm_func(filenames):
-            raw_audio = self.dataset.audio["val"][filename]
-            feature = self.dataset.extract_feature(raw_audio)
-            target = self.dataset.meta["val"].at[filename, "classID"]
-
-            self.X_val.append(feature)
-            self.y_val.append(target)
-
-        self.X_val = np.asarray(self.X_val)
-        self.y_val = np.asarray(self.y_val)
-        return self.X_val, self.y_val
+        if 0 > self.sampling > 1.0:
+            raise AssertionError("Sampling ratio must be between 0 and 1")
 
     def __len__(self):
         nb_file = len(self.filenames)
         return nb_file
 
     def __getitem__(self, index):
-        """
-        Args:
-            index:
-        """
         filename = self.filenames[index]
         return self._generate_data(filename)
 
     def _generate_data(self, filename: str):
-        """
-        Args:
-            filename (str):
-        """
-        LENGTH = DatasetManager.LENGTH
-        SR = self.dataset.sr
-
         # load the raw_audio
         raw_audio = self.x[filename]
 
         # recover ground truth
         y = self.y.at[filename, "classID"]
 
-        # signal data augmentation
-        np.random.shuffle(self.augments)
-        for augment_func in self.augments:
-            if isinstance(augment_func, SignalAugmentation):
-                raw_audio = augment_func(raw_audio)
+        raw_audio = self._apply_signal_augmentation(raw_audio)
+        raw_audio = self._pad_and_crop(raw_audio)
 
-        # padding and cropping
+        # extract feature and apply spec augmentation
+        feat = self.dataset.extract_feature(raw_audio)
+        feat = self._apply_spec_augmentation(feat)
+        y = np.asarray(y)
+
+        return feat, y
+
+    def _pad_and_crop(self, raw_audio):
+        LENGTH = DatasetManager.LENGTH
+        SR = self.dataset.sr
+
         if len(raw_audio) < LENGTH * SR:
             missing = (LENGTH * SR) - len(raw_audio)
             raw_audio = np.concatenate((raw_audio, [0] * missing))
@@ -92,17 +79,23 @@ class Generator(data.Dataset):
         if len(raw_audio) > LENGTH * SR:
             raw_audio = raw_audio[:LENGTH * SR]
 
-        # extract feature
-        feat = self.dataset.extract_feature(raw_audio)
-        y = np.asarray(y)
+        return raw_audio
 
-        # spectro data augmentation
+    def _apply_signal_augmentation(self, raw_audio):
+        np.random.shuffle(self.augments)
+        for augment_func in self.augments:
+            if isinstance(augment_func, SignalAugmentation):
+                raw_audio = augment_func(raw_audio)
+
+        return raw_audio
+
+    def _apply_spec_augmentation(self, feature):
         np.random.shuffle(self.augments)
         for augment_func in self.augments:
             if isinstance(augment_func, SpecAugmentation):
-                feat = augment_func(feat)
+                feature = augment_func(feature)
 
-        return feat, y
+        return feature
 
 
 class CoTrainingGenerator(data.Dataset):
@@ -278,123 +271,3 @@ class CoTrainingGenerator(data.Dataset):
         # Convert to np array
         return np.array(features), np.array(targets)
 
-
-class CoTrainingGenerator_SA(CoTrainingGenerator):
-    def __init__(self, dataset, sampler, unlabel_target: bool = False, augments: tuple = ()):
-        super().__init__(dataset, sampler, unlabel_target, augments)
-
-    def __getitem__(self, batch_idx):
-        raw_X, y = super().__getitem__(batch_idx)
-
-        views_indexes = batch_idx[:-1]
-        U_indexes = batch_idx[-1]
-
-        # Prepare views
-        augmentations = dict()
-        for augment in self.augments:
-            X = []
-
-            # prepare for each view
-            for vi in views_indexes:
-                X_V, y_V = self._generate_data(
-                    vi,
-                    target_filenames=self.filenames_S,
-                    target_raw=self.dataset.augmentations[augment],
-                    target_meta=self.y_S,
-                )
-                X.append(X_V)
-
-            # Prepare U
-            target_meta = None if self.unlabel_target else self.y_U
-            X_U, y_U = self._generate_data(
-                U_indexes,
-                target_filenames=self.filenames_U,
-                target_raw=self.dataset.augmentations[augment],
-                target_meta=target_meta
-            )
-            X.append(X_U)
-
-            augmentations[augment] = X
-
-        return raw_X, y, augmentations
-
-    def _generate_data(self, indexes: list, target_filenames: list, target_raw: dict, target_meta: pd.DataFrame = None):
-        """
-        Args:
-            indexes (list):
-            target_filenames (list):
-            target_raw (dict):
-            target_meta (pd.DataFrame):
-        """
-        LENGTH = DatasetManager.LENGTH
-        SR = self.dataset.sr
-        # Get the corresponding filenames
-        filenames = [target_filenames[i] for i in indexes]
-
-        # Get the raw_adio
-        raw_audios = [target_raw[name] for name in filenames]
-
-        # Get the ground truth
-        # For unlabel set, create a fake ground truth (batch collate error otherwise)
-        targets = 0 #[[0] * DatasetManager.NB_CLASS for _ in range(len(filenames))]
-        if target_meta is not None:
-            targets = [target_meta.at[name, "classID"] for name in filenames]
-
-        # Padding and cropping
-        for i in range(len(raw_audios)):
-            if len(raw_audios[i]) < LENGTH * SR:
-                missing = (LENGTH * SR) - len(raw_audios[i])
-                raw_audios[i] = np.concatenate((raw_audios[i], [0] * missing))
-
-            if len(raw_audios[i]) > LENGTH * SR:
-                raw_audios[i] = raw_audios[i][:LENGTH * SR]
-
-        # Extract the features
-        features = [self.dataset.extract_feature(raw) for raw in raw_audios]
-
-        # Convert to np array
-        return np.array(features), np.array(targets)
-
-if __name__ == '__main__':
-    from datasetManager import DatasetManager
-    from samplers import CoTrainingSampler
-
-    # load the data
-    audio_root = "../dataset/audio"
-    metadata_root = "../dataset/metadata"
-    #dataset = DatasetManager(metadata_root, audio_root, verbose=1)
-
-    augmentation_to_use = ("PitchShiftChoice", "Noise")
-    dataset = DatasetManager(metadata_root, audio_root,
-                             train_fold=[1],
-                             hdf_augments_file="urbansound8k_22050_default_config_1.hdf5",
-                             augments=augmentation_to_use
-                             )
-
-    # prepare the sampler with the specified number of supervised file
-    nb_train_file = len(dataset.audio["train"])
-    nb_s_file = nb_train_file // 10
-    nb_s_file = nb_s_file - (nb_s_file % DatasetManager.NB_CLASS)  # need to be a multiple of number of class
-    nb_u_file = nb_train_file - nb_s_file
-    sampler = CoTrainingSampler(32, nb_s_file, nb_u_file, nb_view=2, ratio=None, method="duplicate")
-
-    # create the generator and the loader
-    generator = CoTrainingGenerator_SA(dataset, sampler, augments=augmentation_to_use)
-
-    train_loader = data.DataLoader(generator, batch_sampler=sampler)
-
-    for X, y, augmented in train_loader:
-        X = [x.squeeze() for x in X]
-        y = [y_.squeeze() for y_ in y]
-        for key in augmented:
-            augmented[key] = [x.squeeze() for x in augmented[key]]
-
-        # separate Supervised (S) and Unsupervised (U) parts
-        X_S, X_U = X[:-1], X[-1]
-        y_S, y_U = y[:-1], y[-1]
-
-        print("X_U shape: ", X_U.shape)
-        print("X_S shape: ", X_S[0].shape)
-        print("y_U shape: ", y_U.shape)
-        print("y_S shape: ", y_S[0].shape)
-        break
