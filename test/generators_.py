9ed248eea3ed64971efcb82ec6e3f5de89e8e922
leocances <leocances@gmail.com>
2020-02-10 09:45:44 +0100

    add test for the cache system (working well)

--- test/generators_.py
+++ test/generators_.py
@@ -22,8 +22,8 @@ metadata_root = "../dataset/metadata"
 dataset = DatasetManager(metadata_root, audio_root, train_fold=[1], val_fold=[2])
 
 # test generator train / val
-train_loader = Generator(dataset, train=True, val=False, sampling=1.0, augments=[])
-val_loader = Generator(dataset, train=False, val=True, sampling=1.0, augments=[])
+train_loader = Generator(dataset, train=True, val=False, sampling=1.0, augments=["test"], cached=True)
+val_loader = Generator(dataset, train=False, val=True, sampling=1.0, augments=[], cached=True)
 
 # test really different
 print(np.array(train_loader.y.index.values) == np.array(val_loader.y.index.values))
@@ -33,7 +33,8 @@ print(np.array(train_loader.y.index.values) == np.array(val_loader.y.index.value
 def extract_all_files(loader):
     for i in tqdm.tqdm(range(len(loader))):
         x = loader[i]
-        
+
+
 extract_all_files(train_loader)
 extract_all_files(train_loader)
 extract_all_files(val_loader)
