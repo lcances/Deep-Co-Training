230ce4ae0ae09aa4727319339c2aecd7661f1741
leocances <leocances@gmail.com>
2020-02-12 14:23:53 +0100

    fix minor error

--- src/utils.py
+++ src/utils.py
@@ -14,6 +14,6 @@ def get_model_from_name(model_name):
     for name, obj in inspect.getmembers(models):
         if inspect.isclass(obj) or inspect.isfunction(obj):
             if obj.__name__ == model_name:
-                logging.info("Model loaded: %s" % model_func.__name__)
+                logging.info("Model loaded: %s" % model_name)
                 return obj
     raise AttributeError("This model does not exist: %s " % model_name)
