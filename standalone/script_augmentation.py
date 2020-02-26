66129ca31d7a7c5db57eb41edf9190a9bbd75863
leocances <leocances@gmail.com>
2020-02-26 13:57:24 +0100

    script for performing grid search for augmentations

---
+++ standalone/script_augmentation.py
@@ -0,0 +1,49 @@
+import os
+import subprocess
+os.environ["MKL_NUM_THREADS"] = "2"
+os.environ["NUMEXPR_NU M_THREADS"] = "2"
+os.environ["OMP_NUM_THREADS"] = "2"
+import numpy as np
+import tqdm
+import random
+import time
+
+import torch
+import torch.nn as nn
+import torch.utils.data as data
+from torch.optim.lr_scheduler import LambdaLR
+from torch.utils.tensorboard import SummaryWriter
+
+import sys
+sys.path.append("../src/")
+
+from datasetManager import DatasetManager
+from generators import Dataset
+from utils import get_datetime, get_model_from_name
+from metrics import CategoricalAccuracy
+import signal_augmentations
+import spec_augmentations
+import img_augmentations
+
+
+# Unique augmentation to execute
+unique_augments=dict(
+    psc1    = "signal_augmentations.PitchShiftChoice(0.5, choice=(-3, -2, 2, 3))",
+    psc2    = "signal_augmentations.PitchShiftChoice(0.5, choice=(-1.5, -1, 1, 1.5))",
+    l1      = "signal_augmentations.Level(0.5, rate=(0.9, 1.1))",
+    l2      = "signal_augmentations.Level(0.5, rate=(0.8, 1.2))",
+    n1      = "signal_augmentations.Noise(0.5, target_snr=15)",
+    n2      = "signal_augmentations.Noise(0.5, target_snr=20)",
+    n3      = "signal_augmentations.Noise(0.5, target_snr=25)",
+    rfd01   = "spec_augmentations.RandomFreqDropout(0.5, dropout=0.1)",
+    rfd0075 = "spec_augmentations.RandomFreqDropout(0.5, dropout=0.075)",
+    rfd02   = "spec_augmentations.RandomFreqDropout(0.5, dropout=0.2)",
+    sn25    = "spec_augmentations.Noise(1.0, 25)",
+    rfd005  = "spec_augmentations.RandomFreqDropout(0.5, dropout=0.05)",
+)
+
+
+# execution
+print(sys.argv)
+for key, aug in unique_augments.items():
+    subprocess.call(["python", "full_supervised_aug.py", *sys.argv[1:], "-a", "\"%s\"" % aug])
