81b7ef81c03c9ffad6c0a974bf96d4701fa4c65a
leocances <leocances@gmail.com>
2020-03-26 14:51:46 +0100

    fix error to run with sbatch

--- osirim/script_crossval_dct_moreS.sh
+++ osirim/script_crossval_dct_moreS.sh
@@ -1,8 +1,8 @@
-#!/bin/sh
+#!/bin/bash
 
 #SBATCH --job-name=crossval_dct_moreS
-#SBATCH --output=cross_val_dct_more2.out
-#SBATCH --error=cross_val_dct_moreS.err
+#SBATCH --output=crossval_dct_moreS_%j.out
+#SBATCH --error=crossval_dct_moreS_%j.err
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=5
 #SBATCH --partition=GPUNodes
@@ -14,8 +14,9 @@ container=/logiciels/containerCollections/CUDA10/pytorch.sif
 python=/users/samova/lcances/.miniconda3/envs/dl/bin/python
 script=../standalone/co-training.py
 
-if [ #1 -eq 2 ]; then
+if [ "$#" -ne 2 ]; then
   echo "wrong number of parameter"
+  echo "received $# arguments"
   echo "usage: [parser_ratio] [model]"
   exit 1
 fi
@@ -23,15 +24,15 @@ fi
 parser_ratio="--parser_ratio $1"
 model="--model $2"
 
-if [ "$model" = "cnn" ]; then
+if [ "$2" = "cnn" ]; then
   hyper_parameters="--base_lr 0.01 --lambda_cot_max 5 --lambda_diff_max 0.25 --warm_up 160 --epsilon 0.1"
 fi
 
-if [ "$model" = "scallable2" ]; then
+if [ "$2" = "scallable2" ]; then
   hyper_parameters="--base_lr 0.01 --lambda_cot_max 2 --lambda_diff_max 0.5 --warm_up 120 --epsilon 0.02"
 fi
 
-parameters="${model} ${parser_ratio} ${hyper_parameters}"
+parameters="${model} ${parser_ratio} ${hyper_parameters} -T moreS"
 
 folds=(
 	"-t 2 3 4 5 6 7 8 9 10 -v 1" \
@@ -49,7 +50,7 @@ folds=(
 job_number=1
 for i in ${!folds[*]}
 do
-  job_name="--job_name none_${parser_ratio}pr_run${job_number}"
+  job_name="--job_name none_$1pr_run${job_number}"
   srun -n1 -N1 singularity exec ${container} ${python} ${script} ${parameters} ${folds[$i]} ${job_name}
   job_number=$(( $job_number + 1 ))
 done
