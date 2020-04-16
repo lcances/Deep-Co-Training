8b4eeba688891b5dbd637ceff8493c61e9712018
leocances <leocances@gmail.com>
2020-04-16 11:37:42 +0200

    refactoring to make it like a pip package

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ubs8k',
+    version='1.0.0',
+    url='https://github.com/leocances/UrbanSound8K.git',
+    author='Leo Cances',
+    author_email='leocances@gmail.com',
+    description='work on the UrbanSound8K dataset',
+    packages=find_packages(),
+)
