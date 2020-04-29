0a31330a50518f11adcf93dd723d3f148dd607c8
leocances <leocances@gmail.com>
2020-04-29 17:48:54 +0200

    add the possibility to precalculate augmentation

---
+++ ubs8k/augmentation_list.py
@@ -0,0 +1,17 @@
+# Unique augmentation to execute
+unique_augments=dict(
+    psc1    = "signal_augmentations.PitchShiftChoice(0.5, choice=(-3, -2, 2, 3))",
+    psc2    = "signal_augmentations.PitchShiftChoice(0.5, choice=(-1.5, -1, 1, 1.5))",
+    l1      = "signal_augmentations.Level(0.5, rate=(0.9, 1.1))",
+    l2      = "signal_augmentations.Level(0.5, rate=(0.8, 1.2))",
+    n1      = "signal_augmentations.Noise(0.5, target_snr=15)",
+    n2      = "signal_augmentations.Noise(0.5, target_snr=20)",
+    n3      = "signal_augmentations.Noise(0.5, target_snr=25)",
+    rfd01   = "spec_augmentations.RandomFreqDropout(0.5, dropout=0.1)",
+    rfd0075 = "spec_augmentations.RandomFreqDropout(0.5, dropout=0.075)",
+    rfd02   = "spec_augmentations.RandomFreqDropout(0.5, dropout=0.2)",
+    sn25    = "spec_augmentations.Noise(1.0, 25)",
+    rfd005  = "spec_augmentations.RandomFreqDropout(0.5, dropout=0.05)",
+)
+
+reverse_unique_augment = dict(zip(unique_augments.values(), unique_augments.keys()))
