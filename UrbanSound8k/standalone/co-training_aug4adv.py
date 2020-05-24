b1e9419e0a8032948ce149e2faaff12a422bc399
leocances <leocances@gmail.com>
2020-05-24 20:31:31 +0200

    refactoring

--- UrbanSound8k/standalone/co-training_aug4adv.py
+++ UrbanSound8k/standalone/co-training_aug4adv.py
@@ -17,16 +17,21 @@ from torch.utils.tensorboard import SummaryWriter
 from advertorch.attacks import GradientSignAttack
 
 import sys
-sys.path.append("../ubs8k/")
+sys.path.append("..") # UrbanSound root
+sys.path.append("../..") # to access augmentation_utils
 
 from ubs8k.datasetManager import DatasetManager
 from ubs8k.generators import CoTrainingDataset
 from ubs8k.samplers import CoTrainingSampler
-from ..utils import get_datetime, get_model_from_name, reset_seed, set_logs
 
-from ..losses import loss_cot, p_loss_diff, p_loss_sup
-from ..metrics import CategoricalAccuracy, Ratio
-from ..ramps import Warmup, sigmoid_rampup
+from utils import get_datetime, get_model_from_name, reset_seed, set_logs
+from losses import loss_cot, p_loss_diff, p_loss_sup
+from metrics import CategoricalAccuracy, Ratio
+from ramps import Warmup, sigmoid_rampup
+
+import augmentation_utils.img_augmentations as img_augmentations
+import augmentation_utils.spec_augmentations as spec_augmentations
+import augmentation_utils.signal_augmentations as signal_augmentations
 
 # ---- Arguments ----
 parser = argparse.ArgumentParser(description='Deep Co-Training for Semi-Supervised Image Recognition')
