5e21d93da856ef89f96ef39d904e56b18d11f670
leocances <leocances@gmail.com>
2020-08-04 09:32:27 +0200

    add missing training script for diff variation of the ctraining

--- UrbanSound8k/standalone/co-training/co-training_noAdv.py
+++ UrbanSound8k/standalone/co-training/co-training_noAdv.py
@@ -7,17 +7,22 @@ import time
 import numpy as np
 import torch
 import torch.nn as nn
+import torch.cuda.amp as amp
 import torch.utils.data as data
+import torch.nn.functional as F
 from torch.optim.lr_scheduler import LambdaLR
 from torch.utils.tensorboard import SummaryWriter
 from advertorch.attacks import GradientSignAttack
+
+
+# In[3]:
+
 
 from ubs8k.datasetManager import DatasetManager
 from ubs8k.datasets import Dataset
 
 import sys
-
-sys.path.append("../..")
+sys.path.append("../../..")
 
 from util.utils import reset_seed, get_datetime, get_model_from_name, ZipCycle
 from metric_utils.metrics import CategoricalAccuracy, FScore, ContinueAverage, Ratio
@@ -26,9 +31,13 @@ from util.checkpoint import CheckPoint
 from UrbanSound8k.ramps import Warmup, sigmoid_rampup
 from UrbanSound8k.losses import loss_cot, loss_diff, loss_sup
 
-# Arguments
-import argparse
 
+# # Arguments
+
+# In[4]:
+
+
+import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument("-d", "--dataset_root", default="../../../datasets/ubs8k", type=str)
 parser.add_argument("--supervised_ratio", default=0.1, type=float)
@@ -37,31 +46,29 @@ parser.add_argument("-t", "--train_folds", nargs="+", default=[1, 2, 3, 4, 5, 6,
 parser.add_argument("-v", "--val_folds", nargs="+", default=[10], type=int)
 
 parser.add_argument("--model", default="cnn03", type=str)
-parser.add_argument("--batch_size", default=32, type=int)
+parser.add_argument("--batch_size", default=100, type=int)
 parser.add_argument("--nb_epoch", default=100, type=int)
 parser.add_argument("--learning_rate", default=0.003, type=int)
 
 parser.add_argument("--lambda_cot_max", default=10, type=float)
-parser.add_argument("--lambda_diff_max", default=0.5, type=float)
 parser.add_argument("--warmup_length", default=80, type=int)
-parser.add_argument("--epsilon", default=0.02, type=float)
 
 parser.add_argument("--augment", action="append", help="augmentation. use as if python script")
 parser.add_argument("--augment_S", action="store_true", help="Apply augmentation on Supervised part")
 parser.add_argument("--augment_U", action="store_true", help="Apply augmentation on Unsupervised part")
 
-parser.add_argument("--checkpoint_path", default="../../../model_save/ubs8k/deep-co-training", type=str)
+parser.add_argument("--checkpoint_path", default="../../../model_save/ubs8k/deep-co-training_noAdv", type=str)
 parser.add_argument("--resume", action="store_true", default=False)
-parser.add_argument("--tensorboard_path", default="../../../tensorboard/ubs8k/deep-co-training", type=str)
+parser.add_argument("--tensorboard_path", default="../../../tensorboard/ubs8k/deep-co-training_noAdv", type=str)
 parser.add_argument("--tensorboard_sufix", default="", type=str)
 
 args = parser.parse_args()
 
-# %% md
 
-## Prepare the dataset
+# ## Prepare the dataset
 
-# %%
+# In[5]:
+
 
 audio_root = os.path.join(args.dataset_root, "audio")
 metadata_root = os.path.join(args.dataset_root, "metadata")
@@ -70,20 +77,22 @@ all_folds = args.train_folds + args.val_folds
 manager = DatasetManager(
     metadata_root, audio_root,
     folds=all_folds,
-    verbose=2
+    verbose=1
 )
 
-# %%
+
+# In[6]:
+
 
 # prepare the sampler with the specified number of supervised file
 train_dataset = Dataset(manager, folds=args.train_folds, cached=True)
 val_dataset = Dataset(manager, folds=args.val_folds, cached=True)
 
-# %% md
 
-## Models
+# ## Models
 
-# %%
+# In[7]:
+
 
 torch.cuda.empty_cache()
 model_func = get_model_from_name(args.model)
@@ -93,26 +102,8 @@ m1, m2 = model_func(manager=manager), model_func(manager=manager)
 m1 = m1.cuda()
 m2 = m2.cuda()
 
-# %% md
 
-## Adversarial generator
-
-# %%
-
-# adversarial generation
-adv_generator_1 = GradientSignAttack(
-    m1, loss_fn=nn.CrossEntropyLoss(reduction="sum"),
-    eps=args.epsilon, clip_min=-80, clip_max=0, targeted=True
-)
-
-adv_generator_2 = GradientSignAttack(
-    m2, loss_fn=nn.CrossEntropyLoss(reduction="sum"),
-    eps=args.epsilon, clip_min=-80, clip_max=0, targeted=True
-)
-
-# %% md
-
-## Loaders
+# ## Loaders
 s_idx, u_idx = train_dataset.split_s_u(args.supervised_ratio)
 
 # Calc the size of the Supervised and Unsupervised batch
@@ -136,10 +127,15 @@ train_loader_u = data.DataLoader(train_dataset, batch_size=u_batch_size, sampler
 train_loader = ZipCycle([train_loader_s1, train_loader_s2, train_loader_u])
 val_loader = data.DataLoader(val_dataset, batch_size=args.batch_size, shuffle=True)
 
-# training parameters
+
+# ## training parameters
+
+# In[9]:
+
+
 # tensorboard
-tensorboard_title = "%s_%s_%.1f" % (get_datetime(), model_func.__name__, args.supervised_ratio)
-checkpoint_title = "%s_%.1f" % (model_func.__name__, args.supervised_ratio)
+tensorboard_title = "%s_%s_%.1fS_%s" % (get_datetime(), model_func.__name__, args.supervised_ratio, args.tensorboard_sufix)
+checkpoint_title = "%s_%.1fS" % (model_func.__name__, args.supervised_ratio)
 tensorboard = SummaryWriter(log_dir="%s/%s" % (args.tensorboard_path, tensorboard_title), comment=model_func.__name__)
 
 # Losses
@@ -148,15 +144,15 @@ tensorboard = SummaryWriter(log_dir="%s/%s" % (args.tensorboard_path, tensorboar
 # Optimizer
 params = list(m1.parameters()) + list(m2.parameters())
 optimizer = torch.optim.Adam(params, lr=args.learning_rate)
+grad_scaler = amp.GradScaler()
 
 # define the warmups
 lambda_cot = Warmup(args.lambda_cot_max, args.warmup_length, sigmoid_rampup)
-lambda_diff = Warmup(args.lambda_diff_max, args.warmup_length, sigmoid_rampup)
 
 # callback
-lr_lambda = lambda epoch: (1.0 + np.cos((epoch - 1) * np.pi / args.nb_epoch))
+lr_lambda = lambda epoch: (1.0 + np.cos((epoch-1) * np.pi / args.nb_epoch))
 lr_scheduler = LambdaLR(optimizer, lr_lambda)
-callbacks = [lr_scheduler, lambda_cot, lambda_diff]
+callbacks = [lr_scheduler, lambda_cot]
 
 # checkpoints
 checkpoint_m1 = CheckPoint(m1, optimizer, mode="max", name="%s/%s_m1.torch" % (args.checkpoint_path, checkpoint_title))
@@ -177,7 +173,6 @@ metrics_fn = dict(
     avg_diff=ContinueAverage(),
 )
 
-
 def reset_metrics():
     for item in metrics_fn.values():
         if isinstance(item, list):
@@ -185,41 +180,47 @@ def reset_metrics():
                 f.reset()
         else:
             item.reset()
+
+
 reset_metrics()
 
-## Can resume previous training
+
+# ## Can resume previous training
 if args.resume:
     checkpoint_m1.load_last()
 
 
-## Metrics and hyperparameters
+# ## Metrics and hyperparameters
+
+# In[12]:
+
+
 def get_lr(optimizer):
     for param_group in optimizer.param_groups:
         return param_group['lr']
 
 
-# %% md
-
-# Training functions
-
-# %%
-
+# # Training functions
 UNDERLINE_SEQ = "\033[1;4m"
 
 RESET_SEQ = "\033[0m"
 
-header_form = "{:<8.8} {:<6.6} - {:<6.6} - {:<8.8} {:<6.6} | {:<6.6} | {:<6.6} | {:<6.6} - {:<9.9} {:<9.9} | {:<9.9}- {:<6.6}"
-value_form = "{:<8.8} {:<6} - {:<6} - {:<8.8} {:<6.4f} | {:<6.4f} | {:<6.4f} | {:<6.4f} - {:<9.9} {:<9.4f} | {:<9.4f}- {:<6.4f}"
+
+header_form = "{:<8.8} {:<6.6} - {:<6.6} - {:<8.8} {:<6.6} | {:<6.6} | {:<6.6} - {:<9.9} {:<9.9} | {:<9.9}- {:<6.6}"
+value_form  = "{:<8.8} {:<6} - {:<6} - {:<8.8} {:<6.4f} | {:<6.4f} | {:<6.4f} - {:<9.9} {:<9.4f} | {:<9.4f}- {:<6.4f}"
 
 header = header_form.format(
-    "", "Epoch", "%", "Losses:", "Lsup", "Lcot", "Ldiff", "total", "metrics: ", "acc_s1", "acc_u1", "Time"
+    "", "Epoch", "%", "Losses:", "Lsup", "Lcot", "total", "metrics: ", "acc_s1", "acc_u1","Time"
 )
+
 
 train_form = value_form
 val_form = UNDERLINE_SEQ + value_form + RESET_SEQ
 
 print(header)
 
+
+# In[14]:
 def maximum():
     def func(key, value):
         if key not in func.max:
@@ -232,6 +233,36 @@ def maximum():
     func.max = dict()
     return func
 maximum_fn = maximum()
+
+def loss_cot_v(U_p1, U_p2):
+    # the Jensen-Shannon divergence between p1(x) and p2(x)
+    S = nn.Softmax(dim=1)
+    LS = nn.LogSoftmax(dim=1)
+    U_batch_size = U_p1.size()[0]
+    eps=1e-8
+
+    a1 = 0.5 * (S(U_p1) + S(U_p2))
+    a1 = torch.clamp(a1, min=eps)
+    loss1 = a1 * torch.log(a1)
+    loss1 = -torch.sum(loss1)
+    print("========================")
+    print("loss1: ", loss1)
+    print(S(U_p1))
+    print(S(U_p2))
+    print(a1)
+    print(torch.log(a1))
+
+    loss2 = S(U_p1) * LS(U_p1)
+    loss2 = -torch.sum(loss2)
+    print("========================")
+    print("loss2: ", loss2)
+
+    loss3 = S(U_p2) * LS(U_p2)
+    loss3 = -torch.sum(loss3)
+    print("========================")
+    print("loss3: ", loss3)
+
+    return (loss1 - 0.5 * (loss2 + loss3)) / U_batch_size
 
 def train(epoch):
     start_time = time.time()
@@ -258,47 +289,26 @@ def train(epoch):
         pred_u1 = torch.argmax(logits_u1, 1)
         pred_u2 = torch.argmax(logits_u2, 1)
 
-        # ======== Generate adversarial examples ========
-        # fix batchnorm ----
-        m1.eval()
-        m2.eval()
-
-        # generate adversarial examples ----
-        adv_data_s1 = adv_generator_1.perturb(x_s1, y_s1)
-        adv_data_u1 = adv_generator_1.perturb(x_u, pred_u1)
-
-        adv_data_s2 = adv_generator_2.perturb(x_s2, y_s2)
-        adv_data_u2 = adv_generator_2.perturb(x_u, pred_u2)
-
-        m1.train()
-        m2.train()
-
-        # predict adversarial examples ----
-        adv_logits_s1 = m1(adv_data_s2)
-        adv_logits_s2 = m2(adv_data_s1)
-
-        adv_logits_u1 = m1(adv_data_u2)
-        adv_logits_u2 = m2(adv_data_u1)
-
         # ======== calculate the differents loss ========
-        # zero the parameter gradients ----
-        optimizer.zero_grad()
-        m1.zero_grad()
-        m2.zero_grad()
-
         # losses ----
         l_sup = loss_sup(logits_s1, logits_s2, y_s1, y_s2)
 
         l_cot = loss_cot(logits_u1, logits_u2)
 
-        l_diff = loss_diff(
-            logits_s1, logits_s2, adv_logits_s1, adv_logits_s2,
-            logits_u1, logits_u2, adv_logits_u1, adv_logits_u2
-        )
+        if np.isnan(l_cot.item()):
+            loss_cot_v(logits_u1, logits_u2)
+            print("l cot is Nan")
+            print("l_cot")
+            print(l_cot)
+            print("logits shape")
+            print(logits_u1.shape)
 
-        total_loss = l_sup + lambda_cot() * l_cot + lambda_diff() * l_diff
-        total_loss.backward()
-        optimizer.step()
+        total_loss = l_sup + lambda_cot() * l_cot
+
+        optimizer.zero_grad()
+        grad_scaler.scale(total_loss).backward()
+        grad_scaler.step(optimizer)
+        grad_scaler.update()
 
         # ======== Calc the metrics ========
         with torch.set_grad_enabled(False):
@@ -311,21 +321,10 @@ def train(epoch):
             acc_u1 = metrics_fn["acc_u"][0](pred_u1, y_u)
             acc_u2 = metrics_fn["acc_u"][1](pred_u2, y_u)
 
-            # ratios  ----
-            adv_pred_s1 = torch.argmax(adv_logits_s1, 1)
-            adv_pred_s2 = torch.argmax(adv_logits_s2, 1)
-            adv_pred_u1 = torch.argmax(adv_logits_u1, 1)
-            adv_pred_u2 = torch.argmax(adv_logits_u2, 1)
-
-            ratio_s1 = metrics_fn["ratio_s"][0](adv_pred_s1, y_s1)
-            ratio_s2 = metrics_fn["ratio_s"][0](adv_pred_s2, y_s2)
-            ratio_u1 = metrics_fn["ratio_s"][0](adv_pred_u1, y_u)
-            ratio_u2 = metrics_fn["ratio_s"][0](adv_pred_u2, y_u)
             # ========
 
             avg_total = metrics_fn["avg_total"](total_loss.item())
             avg_sup = metrics_fn["avg_sup"](l_sup.item())
-            avg_diff = metrics_fn["avg_diff"](l_diff.item())
             avg_cot = metrics_fn["avg_cot"](l_cot.item())
 
             # logs
@@ -333,36 +332,32 @@ def train(epoch):
                 "Training: ",
                 epoch + 1,
                 int(100 * (batch + 1) / len(train_loader)),
-                "", avg_sup.mean, avg_cot.mean, avg_diff.mean, avg_total.mean,
+                "", avg_sup.mean, avg_cot.mean, avg_total.mean,
                 "", acc_s1.mean, acc_u1.mean,
                 time.time() - start_time
             ), end="\r")
 
+
     # using tensorboard to monitor loss and acc\n",
     tensorboard.add_scalar('train/total_loss', avg_total.mean, epoch)
-    tensorboard.add_scalar('train/Lsup', avg_sup.mean, epoch)
-    tensorboard.add_scalar('train/Lcot', avg_cot.mean, epoch)
-    tensorboard.add_scalar('train/Ldiff', avg_diff.mean, epoch)
-    tensorboard.add_scalar("train/acc_1", acc_s1.mean, epoch)
-    tensorboard.add_scalar("train/acc_2", acc_s2.mean, epoch)
+    tensorboard.add_scalar('train/Lsup', avg_sup.mean, epoch )
+    tensorboard.add_scalar('train/Lcot', avg_cot.mean, epoch )
+    tensorboard.add_scalar("train/acc_1", acc_s1.mean, epoch )
+    tensorboard.add_scalar("train/acc_2", acc_s2.mean, epoch )
 
     tensorboard.add_scalar("detail_acc/acc_s1", acc_s1.mean, epoch)
     tensorboard.add_scalar("detail_acc/acc_s2", acc_s2.mean, epoch)
     tensorboard.add_scalar("detail_acc/acc_u1", acc_u1.mean, epoch)
     tensorboard.add_scalar("detail_acc/acc_u2", acc_u2.mean, epoch)
 
-    tensorboard.add_scalar("detail_ratio/ratio_s1", ratio_s1.mean, epoch)
-    tensorboard.add_scalar("detail_ratio/ratio_s2", ratio_s2.mean, epoch)
-    tensorboard.add_scalar("detail_ratio/ratio_u1", ratio_u1.mean, epoch)
-    tensorboard.add_scalar("detail_ratio/ratio_u2", ratio_u2.mean, epoch)
-
     # Return the total loss to check for NaN
-    return total_loss.item(), msg
+    return total_loss.item()
 
 
-# %%
+# In[16]:
 
-def test(epoch, msg=""):
+
+def test(epoch):
     start_time = time.time()
     print("")
 
@@ -395,20 +390,19 @@ def test(epoch, msg=""):
             print(val_form.format(
                 "Validation: ",
                 epoch + 1,
-                int(100 * (batch + 1) / len(train_loader)),
-                "", avg_sup.mean, 0.0, 0.0, avg_sup.mean,
+                int(100 * (batch + 1) / len(val_loader)),
+                "", avg_sup.mean, 0.0, avg_sup.mean,
                 "", acc_1.mean, 0.0,
                 time.time() - start_time
             ), end="\r")
 
     tensorboard.add_scalar("val/acc_1", acc_1.mean, epoch)
     tensorboard.add_scalar("val/acc_2", acc_2.mean, epoch)
-
-    tensorboard.add_scalar("max/acc_1", maximum_fn("acc_1", acc_1.mean), epoch)
-    tensorboard.add_scalar("max/acc_2", maximum_fn("acc_2", acc_2.mean), epoch)
+    
+    tensorboard.add_scalar("max/acc_1", maximum_fn("acc_1", acc_1.mean), epoch )
+    tensorboard.add_scalar("max/acc_2", maximum_fn("acc_2", acc_2.mean), epoch )
 
     tensorboard.add_scalar("detail_hyperparameters/lambda_cot", lambda_cot(), epoch)
-    tensorboard.add_scalar("detail_hyperparameters/lambda_diff", lambda_diff(), epoch)
     tensorboard.add_scalar("detail_hyperparameters/learning_rate", get_lr(optimizer), epoch)
 
     # Apply callbacks
@@ -420,22 +414,25 @@ def test(epoch, msg=""):
     checkpoint_m2.step(acc_2.mean)
 
 
-# %%
-
 print(header)
 
 for epoch in range(0, args.nb_epoch):
-    total_loss, msg = train(epoch)
+    total_loss = train(epoch)
 
     if np.isnan(total_loss):
         print("Losses are NaN, stoping the training here")
         break
 
-    test(epoch, msg)
+    test(epoch)
 
 tensorboard.flush()
 tensorboard.close()
 
-# %%
+
+# # ♫♪.ılılıll|̲̅̅●̲̅̅|̲̅̅=̲̅̅|̲̅̅●̲̅̅|llılılı.♫♪
+
+# In[25]:
+
+
 
 
