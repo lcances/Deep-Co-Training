ab619d110c56ed1e80dde221d7c887d880609741
leocances <leocances@gmail.com>
2020-08-04 09:31:34 +0200

    fix minor error that could cause NaN in Lcot

--- UrbanSound8k/losses.py
+++ UrbanSound8k/losses.py
@@ -20,8 +20,11 @@ def loss_cot(U_p1, U_p2):
     S = nn.Softmax(dim=1)
     LS = nn.LogSoftmax(dim=1)
     U_batch_size = U_p1.size()[0]
+    eps=1e-8
 
     a1 = 0.5 * (S(U_p1) + S(U_p2))
+    a1 = torch.clamp(a1, min=eps)
+    
     loss1 = a1 * torch.log(a1)
     loss1 = -torch.sum(loss1)
 
