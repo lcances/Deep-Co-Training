c934fd6b4dbffd5ffa1e59d0e0893f48e016cd82
leocances <leocances@gmail.com>
2020-08-13 11:00:03 +0200

    add experiment bash script

---
+++ UrbanSound8k/standalone/co-training/cv_to_run.sh
@@ -0,0 +1,2 @@
+./co-training-independant_loss.sh -m cnn03 -e 3000 -s weighted-linear -S 10 -l 0.0005 -c 1 -d 1 -t 1lcm_1ldm
+./co-training-independant_loss.sh -m cnn03 -e 5000 -s weighted-linear -S 10 -l 0.0005 -c 1 -d 1 -t 1lcm_1ldm
