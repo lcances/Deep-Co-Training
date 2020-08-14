8f38e736e0b00dfb3c18fa40d89948f556a847be
leocances <leocances@gmail.com>
2020-08-14 19:38:02 +0200

    updated execution script

--- UrbanSound8k/standalone/co-training/gs_to_run.sh
+++ UrbanSound8k/standalone/co-training/gs_to_run.sh
@@ -1,45 +1,50 @@
 #!/bin/bash
 
-tensorboard_dir="../../../tensorboard/ubs8k/deep-co-training_independant-loss/1lcm_1ldm"
-checkpoint_dir="../../../model_save/ubs8k/deep-co-training_independant-loss/1lcm_1ldm"
-warmup_length="1"
-
-# models=("cnn0" "cnn03")
-# schedulers=("linear" "sigmoid" "weighted-linear" "weighted-sigmoid")
-# learning_rate=("0.003" "0.0005")
-# steps=("5" "10")
-# epochs=("1000")
-# lambda_diff_max=1
-# lambda_cot_max=1
 
 models=("cnn03")
-schedulers=("weighted-linear")
+schedulers=("annealing-cosine" "weighted-annealing-cosine")
 learning_rate=("0.0005")
-steps=("10")
-epochs=("1500" "2000" "3000" "4000" "5000")
-lambda_diff_max=1
-lambda_cot_max=1
+steps=("10" "5000")
+epochs=("1000")
+lambda_diff_max=(1)
+lambda_cot_max=(1)
+warmup_length=(1)
+cycle=(8 12 16)
+beta=(1 2 3)
+
+tensorboard_dir="../../tensorboard/deep-co-training_independant-loss/${lambda_cot_max}lcm_${lambda_diff_max}ldm"
+checkpoint_dir="../../model_save/deep-co-training_independant-loss/${lambda_cot_max}lcm_${lambda_diff_max}ldm"
 
 for m in ${models[@]}; do
-	for s in ${schedulers[@]}; do
-		for l in ${learning_rate[@]}; do
-			for st in ${steps[@]}; do
-				for e in ${epochs[@]}; do
-					echo $m $s $l $st
-					python co-training_independant_loss.py \
-						--tensorboard_path ${tensorboard_dir} \
-						--checkpoint_path ${checkpoint_dir} \
-						--warmup_length ${warmup_length} \
-						--model $m \
-						--loss_scheduler $s \
-						--learning_rate $l \
-						--loss_scheduler_steps $st \
-						--nb_epoch $e \
-						--lambda_cot_max $lambda_cot_max \
-						--lambda_diff_max $lambda_diff_max
-				done
-						
-			done
-		done
-	done
+for s in ${schedulers[@]}; do
+for l in ${learning_rate[@]}; do
+for st in ${steps[@]}; do
+for e in ${epochs[@]}; do
+for lcm in ${lambda_cot_max[@]}; do
+for ldm in ${lambda_diff_max[@]}; do
+for lc in ${cycle[@]}; do
+for lb in ${beta[@]}; do
+	echo $m $s $l $st
+	python co-training_independant_loss.py \
+		--tensorboard_path ${tensorboard_dir} \
+		--checkpoint_path ${checkpoint_dir} \
+		--warmup_length ${warmup_length} \
+		--model $m \
+		--learning_rate $l \
+		--nb_epoch $e \
+		--lambda_cot_max $lcm \
+		--lambda_diff_max $ldm \
+		--loss_scheduler $s \
+		--loss_scheduler_steps $st \
+		--loss_scheduler_cycle $lc \
+		--loss_scheduler_beta $lb
+		
+done			
+done			
+done			
+done			
+done			
+done
+done
+done
 done
