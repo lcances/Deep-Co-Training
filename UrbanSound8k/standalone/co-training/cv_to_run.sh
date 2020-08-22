c8f397b31634e200c9d859a4d3e83f144dc2a3bd
leocances <leocances@gmail.com>
2020-08-22 16:29:04 +0200

    removing previous organization directory UrbanSound8k

--- UrbanSound8k/standalone/co-training/cv_to_run.sh
+++
@@ -1,2 +0,0 @@
-./co-training-independant_loss.sh -m cnn03 -e 3000 -s weighted-linear -S 10 -l 0.0005 -c 1 -d 1 -t 1lcm_1ldm
-./co-training-independant_loss.sh -m cnn03 -e 5000 -s weighted-linear -S 10 -l 0.0005 -c 1 -d 1 -t 1lcm_1ldm
