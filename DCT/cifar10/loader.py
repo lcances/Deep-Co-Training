be6ff089804b9e7748fbf4e454786cfcf3b37f9b
leocances <leocances@gmail.com>
2020-08-25 11:08:23 +0200

    change normalization preprocess

--- DCT/cifar10/loader.py
+++ DCT/cifar10/loader.py
@@ -93,7 +93,7 @@ def load_cifar10_supervised(
     # Prepare the default dataset
     commun_transform = [
         transforms.ToTensor(),
-        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))
+        transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))
     ]
     
     train_transform = transforms.Compose(extra_train_transform + commun_transform)
