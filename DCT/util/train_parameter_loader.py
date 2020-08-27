20cfb8ca410ff17eb97830d17d682e8a5cb69d21
leocances <leocances@gmail.com>
2020-08-27 20:26:54 +0200

    Added ubs8k training parameters loader

--- DCT/util/train_parameter_loader.py
+++ DCT/util/train_parameter_loader.py
@@ -1,16 +1,36 @@
-from DCT.cifar10.train_parameters import supervised, dct
+import torch.optim
+import torch.optim.lr_scheduler as lr_scheduler
 
-def _get_cifar10_optimizer(framework):
+def _get_cifar10_optimizer(framework: str) -> (torch.optim, lr_scheduler):
+    from DCT.cifar10.train_parameters import supervised, dct
+
     if framework == "supervised":
         return supervised()
 
     elif framework == "dct":
-        return dct
+        return dct()
 
-def _get_ubs8k_optimizer(framework):
-    raise NotImplementedError()
+    else:
+        available_framework = ["supervised", "dct"]
+        raise ValueError("Framework %s is not available.\n Available framework: %s" ", ".join(available_framework))
 
-def get_optimizer(dataset, framework):
+def _get_ubs8k_optimizer(framework: str) -> (torch.optim, lr_scheduler):
+    from DCT.ubs8k.train_parameters import supervised, dct, dct_uniloss
+
+    if framework == "supervised"):
+        return supervised()
+
+    elif framework == "dct":
+        return dct()
+
+    elif framework == "dct_uniloss":
+        return dct_uniloss()
+
+    else:
+        available_framework = ["supervised", "dct"]
+        raise ValueError("Framework %s is not available.\n Available framework: %s" ", ".join(available_framework))
+
+def get_optimizer(dataset: str, framework: str) -> (torch.optim, lr_scheduler):
     if dataset == "cifar10":
         return _get_cifar10_optimizer(framework)
     
