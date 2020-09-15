f8a5fa81fb74f081988eda49a2cfa384cee24787
leocances <leocances@gmail.com>
2020-09-15 10:35:21 +0200

    added model from UrbanSound8k datasets

--- DCT/esc/models.py
+++ DCT/esc/models.py
@@ -1,5 +1,12 @@
 
 # =============================================================================
+# UrbanSound8k models
+# =============================================================================
+from DCT.ubs8k.models import cnn0
+from DCT.ubs8k.models_test import cnn03
+from DCT.ubs8k.models_test import cnn06
+
+# =============================================================================
 #    PyTorch RESNET
 # =============================================================================
-from torchvision.models import resnet34, resnet18, resnet50
+from torchvision.models import resnet18, resnet34, resnet50
