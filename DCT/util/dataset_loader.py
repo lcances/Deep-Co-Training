4ce66ee3b8155b89889d602a50043f91ac672ce5
leocances <leocances@gmail.com>
2020-09-15 10:31:48 +0200

    massive refactor. Easier to handle more datasets

--- DCT/util/dataset_loader.py
+++ DCT/util/dataset_loader.py
@@ -1,31 +1,53 @@
+"""
+Each dataset have they specificity and split into Supervised and Unsupervised
+subset is different for most of them. This utility function allow to get the
+train_loader and val_loader for each dataset and framework. 
 
-def load_cifar10_datasets(framework: str, **kwargs):
-    from DCT.cifar10.dataset_loader import load_cifar10_dct, load_cifar10_supervised
+This abstraction layer allow the standalone script to work with any dataset.
+"""
+
+import DCT.ubs8k.dataset_loader as u8
+import DCT.cifar10.dataset_loader as c10
+#import DCT.esc.dataset_loader as esc
+#import DCT.GTZAN.dataset_loader as gtzan
+#import DCT.SpeechCommand.dataset_loader as SC
+
+dataset_mapper = {
+    "ubs8k": {
+        "supervised": u8.load_supervised,
+        "dct": u8.load_dct,
+        "aug4adv": u8.load_dct_aug4adv
+    },
+
+    "cifar10": {
+        "supervised": c10.load_supervised,
+        "dct": c10.load_dct
+    },
+
+    "esc": {
+        "supervised": esc.load_supervised,
+        "dct": esc.load_dct,
+    },
+
+    "gtzan": {
+        "supervised": gtzan.load_supervised,
+        "dct": gtzan.load_dct,
+    },
+
+    "SpeechCommand": {
+        "supervised": SC.load_supervised,
+        "dct": SC.load_dct,
+    },
+}
+
+def load_datasets_helper(framework: str, mapper: dict, **kwargs):
+
+    if framework not in mapper:
+        raise ValueError("Framework %s doesn't exist. Available framewokrs are {%s}" % (list(mapper.keys())))
     
-    if framework == "dct":
-        return load_cifar10_dct(**kwargs)
-
-    elif framework == "supervised":
-        return load_cifar10_supervised(**kwargs)
-
     else:
-        available = ["supervised", "dct"]
-        raise ValueError("framework %s do not exist. Available frameworks %s" % (framework, available))
+        return mapper[framework](**kwargs)
 
-def load_ubs8k_datasets(framework: str):
-    from DCT.ubs8k.dataset_loader import load_ubs8k_dct, load_ubs8k_supervised, load_ubs8k_dct_aug4adv
-        
-    if framework == "dct":
-        return load_ubs8k_dct(**kwargs)
-
-    elif framework == "supervised":
-        return load_ubs8k_supervised(**kwargs)
-
-    elif framework == "aug4adv":
-        return load_ubs8k_dct_aug4adv(**kwargs)
-
-    else:
-        raise ValueError("framework %s do not exist. Available %s" % (framework, available))
 
 def load_dataset(dataset_name: str, framework: str, dataset_root: str,
         supervised_ratio: float = 0.1, batch_size: int = 100,
@@ -36,13 +58,11 @@ def load_dataset(dataset_name: str, framework: str, dataset_root: str,
         supervised_ratio = supervised_ratio,
         batch_size = batch_size,
     )
+
+    if dataset_name not in dataset_mapper:
+        available_dataset = ", ".join(list(dataset_mapper.keys()))
+        raise ValueError("dataset %s is not available.\n Available datasets: {%s}" % (
+            dataset_name, available_dataset
+        ))
     
-    if dataset_name == "ubs8k":
-        return load_ubs8k_datasets(framework, **parameters, **kwargs)
-    
-    elif dataset_name == "cifar10":
-        return load_cifar10_datasets(framework, **parameters, **kwargs)
-    
-    else:
-        available_dataset = [ "ubs8k", "cifar10" ]
-        raise ValueError("dataset %s is not available.\n Available datasets: %s" % ", ".join(available_dataset))
+    return load_datasets_helper(framework, dataset_mapper[dataset_name], **parameters, **kwargs)
