37dde02f54846511a144b8f8fb93f094073075a5
leocances <leocances@gmail.com>
2020-09-22 13:18:32 +0200

    backup

--- DCT/SpeechCommand/train_parameters.py
+++ DCT/SpeechCommand/train_parameters.py
@@ -1,13 +1,6 @@
 import torch.optim as optim
 import numpy as np
 
-from torchaudio.transforms import MelSpectrogram, AmplitudeToDB
-
-transforms = nn.Sequential(
-    MelSpectrogram(sample_rate=44100, n_fft=2048, hop_length=512, n_mels=64), 
-    AmplitudeToDB(),
-)
-
 def supervised(params):
     lr_lambda = lambda epoch: (1.0 + np.cos((epoch-1)*np.pi/args.nb_epoch)) * 0.5
 
