35ec1e9bdc7c863b6067883352147c40877fc1b6
leocances <leocances@gmail.com>
2020-10-13 14:04:01 +0200

    Fix error

--- DCT/optimizer/speechcommand.py
+++ DCT/optimizer/speechcommand.py
@@ -2,9 +2,7 @@
 from torch.optim import Optimizer, Adam
 
 
-def supervised(model,
-               learning_rate: float = 0.001,
-               **kwargs) -> Optimizer:
+def supervised(model, learning_rate: float = 0.001, **kwargs) -> Optimizer:
 
     return Adam(model.parameters(), lr=learning_rate)
 
@@ -14,7 +12,7 @@ def dct(model1, model2,
         **kwargs) -> Optimizer:
 
     parameters = list(model1.parameters()) + list(model2.parameters())
-    return torch.optim.Adam(parameters, lr=learning_rate)
+    return Adam(parameters, lr=learning_rate)
 
 
 def dct_uniloss(model1, model2,
